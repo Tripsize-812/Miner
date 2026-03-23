16aa76b9e0bbeeb5a52e387eacfad49f40701808
Tripsize <Tripsize@DESKTOP-P74HDK4>
2026-03-24 02:43:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Miner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Fuchsia;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Controls.Add(this.button7);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(382, 253);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Location = new System.Drawing.Point(23, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 80);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Fuchsia;
+            this.button2.Location = new System.Drawing.Point(129, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 80);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Fuchsia;
+            this.button3.Location = new System.Drawing.Point(235, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 80);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Fuchsia;
+            this.button4.Location = new System.Drawing.Point(23, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 80);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Fuchsia;
+            this.button5.Location = new System.Drawing.Point(129, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 80);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Fuchsia;
+            this.button6.Location = new System.Drawing.Point(235, 89);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 80);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.AutoSize = true;
+            this.button7.Location = new System.Drawing.Point(23, 175);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 39);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Рестарт";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лаб. раб. N1. Ст. гр. ИСПП 24 1/9 Перова В.Д";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Miner
+{
+    public partial class Form1 : Form
+    {
+        private Button[] buttons;  // Массив для ссылок на кнопки
+        private bool[] hasMine;
+        private int minesCount = 2;
+        private bool gameOver = false;
+
+        public Form1()
+        {
+            InitializeComponent();  // ОСТАВЛЯЕМ, он создает кнопки из дизайнера
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+
+            buttons = new Button[6];
+            hasMine = new bool[6];
+
+            // Заполняем массив существующими кнопками
+            buttons[0] = button1;  // button1 - имя кнопки на форме
+            buttons[1] = button2;
+            buttons[2] = button3;
+            buttons[3] = button4;
+            buttons[4] = button5;
+            buttons[5] = button6;
+
+
+
+            // Размещаем мины случайно
+            Random rand = new Random();
+            for (int i = 0; i < minesCount; i++)
+            {
+                int pos;
+                do
+                {
+                    pos = rand.Next(6);
+                } while (hasMine[pos]);
+                hasMine[pos] = true;
+            }
+
+            // Настраиваем каждую кнопку
+            for (int i = 0; i < 6; i++)
+            {
+                buttons[i].Tag = i;  // Устанавливаем Tag
+                buttons[i].Click += Button_Click;  // Добавляем обработчик
+                buttons[i].BackColor = Color.Fuchsia;
+                buttons[i].Enabled = true;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (gameOver) return;
+
+            Button btn = sender as Button;
+
+            // Проверка на null
+            if (btn == null || btn.Tag == null)
+            {
+                MessageBox.Show("Ошибка: кнопка не инициализирована");
+                return;
+            }
+
+            int index = (int)btn.Tag;
+
+            if (hasMine[index])
+            {
+                btn.Text = "МИНА!";
+                btn.BackColor = Color.Red;
+                this.BackColor = Color.Red;
+                gameOver = true;
+
+                foreach (Button b in buttons)
+                {
+                    b.Enabled = false;
+                }
+
+                MessageBox.Show("Вы наступили на мину! Игра окончена!", "Game Over",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                btn.Text = "Безопасно";
+                btn.BackColor = Color.LightGreen;
+                btn.Enabled = false;
+
+                bool allSafePressed = true;
+                for (int i = 0; i < 6; i++)
+                {
+                    if (!hasMine[i] && buttons[i].Enabled)
+                    {
+                        allSafePressed = false;
+                        break;
+                    }
+                }
+
+                if (allSafePressed)
+                {
+                    gameOver = true;
+                    this.BackColor = Color.LightGreen;
+                    MessageBox.Show("Поздравляю! Вы нашли все безопасные кнопки!", "Победа!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            gameOver = false;
+            this.BackColor = Color.Fuchsia;
+
+            Random rand = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                hasMine[i] = false;
+            }
+
+            for (int i = 0; i < minesCount; i++)
+            {
+                int pos;
+                do
+                {
+                    pos = rand.Next(6);
+                } while (hasMine[pos]);
+                hasMine[pos] = true;
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                buttons[i].Text = "";
+                buttons[i].BackColor = Color.Fuchsia;
+                buttons[i].Enabled = true;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
